98a968a030d74ec4880bf7a935a528246a3908a4
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-10-19 01:08:18 +0800

    Added google cloud module

--- android/HealthDroid/app/src/androidTest/java/com/ntu/phongnt/healthdroid/ApplicationTest.java
+++ android/HealthDroid/app/src/androidTest/java/com/ntu/phongnt/healthdroid/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.phongnt.healthdroid;
+package com.ntu.phongnt.healthdroid;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/MainActivity.java
+++ android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.phongnt.healthdroid;
+package com.ntu.phongnt.healthdroid;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
