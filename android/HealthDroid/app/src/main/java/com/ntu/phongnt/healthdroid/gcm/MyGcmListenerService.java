14754c3a6540e93b49742ac491602114000caf3e
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-10-31 04:19:59 +0800

    Add notification

--- android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/gcm/MyGcmListenerService.java
+++ android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/gcm/MyGcmListenerService.java
@@ -1,8 +1,12 @@
 package com.ntu.phongnt.healthdroid.gcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.gms.gcm.GcmListenerService;
+import com.ntu.phongnt.healthdroid.R;
 
 public class MyGcmListenerService extends GcmListenerService{
     private static final String TAG = "MyGcmListenerService";
@@ -11,5 +15,11 @@ public class MyGcmListenerService extends GcmListenerService{
     public void onMessageReceived(String from, Bundle data) {
         Log.d(TAG, from);
         Log.d(TAG, data.getString("reading"));
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.powered_by_google_dark)
+                .setContentTitle("My Tittle")
+                .setContentText("My Content").build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
