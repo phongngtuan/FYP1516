2a282dccf423e8010a47c2afde5c72b5d8fa9c95
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-12-06 16:09:08 +0700

    WIP - access database in GraphFragment

--- android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/fragments/GraphFragment.java
+++ android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/fragments/GraphFragment.java
@@ -1,13 +1,17 @@
 package com.ntu.phongnt.healthdroid.fragments;
 
 import android.app.Fragment;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.DropBoxManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.FrameLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -18,11 +22,13 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.ntu.phongnt.healthdroid.R;
+import com.ntu.phongnt.healthdroid.db.DatabaseHelper;
 
 import java.util.ArrayList;
 
 public class GraphFragment extends Fragment {
-    private LineChart chart;
+    private LineChart chart = null;
+    private DatabaseHelper db = null;
 
     public GraphFragment() {
         super();
@@ -45,8 +51,10 @@ public class GraphFragment extends Fragment {
         chart.setDrawGridBackground(false);
         chart.setBackgroundColor(Color.LTGRAY);
 
-        addEntry();
-        addEntry();
+        db = DatabaseHelper.getInstance(getActivity());
+
+        new LoadCursorTask().execute();
+
         XAxis x1 = chart.getXAxis();
         x1.setDrawGridLines(false);
         x1.setAvoidFirstLastClipping(true);
@@ -58,6 +66,36 @@ public class GraphFragment extends Fragment {
 
         chart_container.addView(chart);
         return view;
+    }
+
+    abstract private class BaseTask<T> extends AsyncTask<T, Void, Cursor> {
+        protected Cursor doQuery() {
+            Cursor result =
+                    db
+                            .getReadableDatabase()
+                            .query(DatabaseHelper.TABLE,
+                                    new String[]{"ROWID AS _id",
+                                            DatabaseHelper.VALUE},
+                                    null, null, null, null, DatabaseHelper.VALUE);
+            Log.i("GraphFragment", String.valueOf(result.getCount()));
+            return (result);
+        }
+    }
+
+    private class LoadCursorTask extends BaseTask<Void> {
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            Log.i("GraphFragment", String.valueOf(cursor.getCount()));
+            Log.i("GraphFragment", chart.toString());
+            addEntry();
+            addEntry();
+            chart.invalidate();
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            return (doQuery());
+        }
     }
 
     private void addEntry() {
