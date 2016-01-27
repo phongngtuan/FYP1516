324a2abd239aba3cbda81c695cc46043df503a3a
Phong Nguyen <phongnt.ptnk@gmail.com>
2016-01-28 01:21:08 +0800

    Add subscription service to access central data for subscription

---
+++ android/HealthDroid/app/src/main/java/com/ntu/phongnt/healthdroid/services/subscription/SubscriptionService.java
@@ -0,0 +1,56 @@
+package com.ntu.phongnt.healthdroid.services.subscription;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+
+public class SubscriptionService extends IntentService {
+    // IntentService can perform
+    private static final String ACTION_ADD_SUBSCRIBED_USER =
+            "com.ntu.phongnt.healthdroid.services.subscription.action.add_subscribed_user";
+    private static final String ACTION_REMOVE_SUBSCRIBED_USER =
+            "com.ntu.phongnt.healthdroid.services.subscription.action.remove_subscribed_user";
+
+    private static final String EXTRA_PARAM_USER =
+            "com.ntu.phongnt.healthdroid.services.subscription.param.user";
+
+    public SubscriptionService() {
+        super("SubscriptionService");
+    }
+
+    public static void addSubscribedUser(Context context, String user) {
+        Intent intent = new Intent(context, SubscriptionService.class);
+        intent.setAction(ACTION_ADD_SUBSCRIBED_USER);
+        intent.putExtra(EXTRA_PARAM_USER, user);
+        context.startService(intent);
+    }
+
+    public static void removeSubscribedUser(Context context, String user) {
+        Intent intent = new Intent(context, SubscriptionService.class);
+        intent.setAction(ACTION_REMOVE_SUBSCRIBED_USER);
+        intent.putExtra(EXTRA_PARAM_USER, user);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_ADD_SUBSCRIBED_USER.equals(action)) {
+                final String user = intent.getStringExtra(EXTRA_PARAM_USER);
+                addSubscribedUser(user);
+            } else if (ACTION_REMOVE_SUBSCRIBED_USER.equals(action)) {
+                final String user = intent.getStringExtra(EXTRA_PARAM_USER);
+                removeSubscribedUser(user);
+            }
+        }
+    }
+
+    private void addSubscribedUser(String user) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void removeSubscribedUser(String user) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
